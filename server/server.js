2e123cd925960fb61c7f34512a3e3137e0dc3de5
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-13 01:10:22 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,8 +9,10 @@ const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
+const PRE_PROMPT = process.env.PRE_PROMPT || "This is a pre-prompt message: ";
+
+
 const openai = new OpenAIApi(configuration);
-const PRE_PROMPT = "This is a pre-prompt message: ";
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -28,7 +30,7 @@ app.post('/', async (req, res) => {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: `${PRE_PROMPT}${prompt}`,
-      temperature: 0.9, // Higher values means the model will take more risks.
+      temperature: 0, // Higher values means the model will take more risks.
       max_tokens: 1000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
       top_p: 1, // alternative to sampling with temperature, called nucleus sampling
       frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
