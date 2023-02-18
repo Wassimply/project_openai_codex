40206b8a493fbb9d22761d663737b2802ef7938e
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 19:50:32 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-import express from 'express';
+ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import axios from 'axios';
@@ -14,13 +14,6 @@ app.use(cors());
 // Parse incoming JSON requests
 app.use(express.json());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  next();
-});
-
 // POST endpoint to receive question from client
 app.post('/question', async (req, res) => {
   try {
@@ -29,6 +22,7 @@ app.post('/question', async (req, res) => {
     // Construct Airtable API URL with filter formula
     const airtableUrl = `https://api.airtable.com/v0/appolcoyLfSXX3Xhy/QA?maxRecords=1&filterByFormula=AND({Question}="${question}")`;
 
+
     // Send GET request to Airtable API with authentication headers
     const response = await axios.get(airtableUrl, {
       headers: {
@@ -36,30 +30,21 @@ app.post('/question', async (req, res) => {
       },
     });
 
-    // If the response is successful and has at least one record, send back the answer field value as a JSON response
-    if (response.status === 200 && response.data.records && response.data.records.length > 0) {
+    // If the response is successful, send back the answer field value as a JSON response
+    if (response.status === 200) {
       const answer = response.data.records[0].fields.Answer;
       res.json({ answer });
     } else {
-      // If the question does not exist in the Airtable table, create a default answer object and send it as a JSON response
-      const defaultAnswer = {
-        records: [
-          {
-            id: '',
-            createdTime: '',
-            fields: {
-              Question: question,
-              Answer: "idk but i'll make sure to dig deeper into it and get back to you next time!",
-            },
-          },
-        ],
-      };
-      setTimeout(() => {
-        res.json(defaultAnswer);
-      }, 10000);
+      // Otherwise, send an error response
+      res.status(response.status).json({ error: 'An error occurred while fetching the answer.' });
     }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An internal server error occurred.' });
   }
 });
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}.`);
+});) 
