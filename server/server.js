59e27e95d6e0f973b7192afa35b86de1ec110130
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 17:53:02 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -17,50 +17,29 @@ app.use(express.json());
 // POST endpoint to receive question from client
 app.post('/question', async (req, res) => {
   try {
-    const question = req.body.prompt;
+    const question = req.body.question;
 
-    // Send POST request to Airtable API to insert a new record
-    const insertUrl = `https://api.airtable.com/v0/appolcoyLfSXX3Xhy/QA`;
-    const insertData = { records: [ { fields: { Question: question } } ] };
-    const insertResponse = await axios.post(insertUrl, insertData, {
+    // Construct Airtable API URL with filter formula
+    const airtableUrl = `https://api.airtable.com/v0/appolcoyLfSXX3Xhy//QA?maxRecords=1&filterByFormula=AND({Question}="${question}")`;
+
+    // Send GET request to Airtable API with authentication headers
+    const response = await axios.get(airtableUrl, {
       headers: {
-        Authorization: `Bearer keyO4UTbHbZ9n0vui`,
-        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
       },
     });
 
-    // Get the ID of the newly inserted record
-    const recordId = insertResponse.data.records[0].id;
-
-    // Poll the Airtable API until the "Answer" field is populated
-    const startTime = new Date();
-    let answer = null;
-    while (!answer && new Date() - startTime < 60000) {
-      const selectUrl = `https://api.airtable.com/v0/appolcoyLfSXX3Xhy/QA/${recordId}`;
-      const selectResponse = await axios.get(selectUrl, {
-        headers: {
-          Authorization: `Bearer keyO4UTbHbZ9n0vui`,
-        },
-      });
-      answer = selectResponse.data.fields.Answer;
-      await new Promise(resolve => setTimeout(resolve, 5000));
-    }
-
-    // If the "Answer" field is populated, send it back to the client as a JSON response
-    if (answer) {
+    // If the response is successful, send back the answer field value as a JSON response
+    if (response.status === 200) {
+      const answer = response.data.records[0].fields.Answer;
       res.json({ answer });
     } else {
       // Otherwise, send an error response
-      res.status(500).json({ error: 'Timed out while waiting for the answer to be added to the database.' });
+      res.status(response.status).json({ error: 'An error occurred while fetching the answer.' });
     }
   } catch (error) {
-    if (error.response && error.response.status === 401) {
-      console.error('Airtable API key is invalid or missing.');
-      res.status(500).json({ error: 'An internal server error occurred.' });
-    } else {
-      console.error(error);
-      res.status(500).json({ error: 'An internal server error occurred.' });
-    }
+    console.error(error);
+    res.status(500).json({ error: 'An internal server error occurred.' });
   }
 });
 
