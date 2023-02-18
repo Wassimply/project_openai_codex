fbb108042ba5c4ead68a98027ec265db8406d33e
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 19:30:32 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,26 +1,3 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import axios from 'axios';
-
-dotenv.config();
-
-const app = express();
-const port = process.env.PORT || 3000;
-
-// Enable CORS middleware
-app.use(cors());
-
-// Parse incoming JSON requests
-app.use(express.json());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  next();
-});
-
 // POST endpoint to receive question from client
 app.post('/question', async (req, res) => {
   try {
@@ -41,25 +18,25 @@ app.post('/question', async (req, res) => {
       const answer = response.data.records[0].fields.Answer;
       res.json({ answer });
     } else {
-      // If the question does not exist in the Airtable table, wait for 30 seconds and then return the response with a default message
-      const defaultAnswer = "idk but i'll make sure to dig deeper into it and get back to you next time!";
+      // If the question does not exist in the Airtable table, create a default answer object and send it as a JSON response
+      const defaultAnswer = {
+        records: [
+          {
+            id: '',
+            createdTime: '',
+            fields: {
+              Question: question,
+              Answer: "idk but i'll make sure to dig deeper into it and get back to you next time!",
+            },
+          },
+        ],
+      };
       setTimeout(() => {
-        res.json({ answer: defaultAnswer });
-      }, 30000);
+        res.json(defaultAnswer);
+      }, 10000);
     }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An internal server error occurred.' });
   }
-});
-
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ error: 'An internal server error occurred.' });
-});
-
-// Start the server
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}.`);
 });
