cdafea12a6ed9d776caf871c5d053e2617f15059
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 11:53:05 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,18 +1,10 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
 import cors from 'cors'
-import { Configuration, OpenAIApi } from 'openai'
+import axios from 'axios'
 
 dotenv.config()
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-// const PRE_PROMPT = process.env.PRE_PROMPT || "This is a pre-prompt message: ";
-
-
-const openai = new OpenAIApi(configuration);
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -25,20 +17,17 @@ app.get('/', async (req, res) => {
 
 app.post('/', async (req, res) => {
   try {
-    const prompt = req.body.prompt;
-
-    const response = await openai.createCompletion({
-      model: "text-davinci-003",
-      prompt: `${prompt}`,
-      temperature: 0, // Higher values means the model will take more risks.
-      max_tokens: 1000, // The maximum number of tokens to generate in the completion. Most models have a context length of 2048 tokens (except for the newest models, which support 4096).
-      top_p: 1, // alternative to sampling with temperature, called nucleus sampling
-      frequency_penalty: 0.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-      presence_penalty: 0, // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    });
+    const question = req.body.prompt;
+    const airtableUrl = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/QA?maxRecords=1&filterByFormula=AND({Question}="${question}")`;
+    const headers = {
+      Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
+    };
+    const response = await axios.get(airtableUrl, { headers });
+    const record = response.data.records[0];
+    const answer = record.fields.Answer;
 
     res.status(200).send({
-      bot: response.data.choices[0].text
+      bot: answer
     });
 
   } catch (error) {
