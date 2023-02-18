0d98db99a2c8fda25a37589e5f644664a95b9bb7
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 19:32:23 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,3 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import axios from 'axios';
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Enable CORS middleware
+app.use(cors());
+
+// Parse incoming JSON requests
+app.use(express.json());
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  next();
+});
+
 // POST endpoint to receive question from client
 app.post('/question', async (req, res) => {
   try {
