c9955c3cbc802df1d8feac737d3908fdd73fbff5
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 13:10:32 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,46 +1,35 @@
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const axios = require("axios");
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
 const app = express();
-const port = process.env.PORT || 3000;
-const airtableUrl = `https://api.airtable.com/v0/${process.env.AIRTABLE_BASE_ID}/QA`;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post("/webhook", async (req, res) => {
-  const question = req.body.message.text;
+const AIRTABLE_BASE_ID = process.env.AIRTABLE_BASE_ID;
+const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
 
-  const airtableParams = {
-    maxRecords: 1,
-    filterByFormula: `AND({Question}="${question}")`,
-  };
-
+app.post('/question', async (req, res) => {
   try {
-    const { data } = await axios.get(airtableUrl, {
-      params: airtableParams,
+    const question = req.body.question;
+    const airtableUrl = `https://api.airtable.com/v0/${AIRTABLE_BASE_ID}/QA?maxRecords=1&filterByFormula=AND({Question}="${question}")`;
+
+    const response = await axios.get(airtableUrl, {
       headers: {
-        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+        Authorization: `Bearer ${AIRTABLE_API_KEY}`,
       },
     });
 
-    const botResponse = data.records.length > 0 ? data.records[0].fields.Answer : "I'm sorry, I don't understand your question.";
-
-    const response = {
-      bot: {
-        name: "AirtableBot",
-        response: botResponse,
-      },
-    };
-
-    res.send(response);
+    const answer = response.data.records[0].fields.Answer;
+    res.json({ answer });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.sendStatus(500);
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
