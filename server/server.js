cdec410842ecc160e0875435ed35ef84b77db7f1
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 20:11:40 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -20,29 +20,54 @@ app.post('/question', async (req, res) => {
     const question = req.body.question;
 
     // Construct Airtable API URL with filter formula
-    const airtableUrl = `https://api.airtable.com/v0/appolcoyLfSXX3Xhy/QA?maxRecords=1&filterByFormula=AND({Question}="${question}")`;
+    const airtableUrl = `https://api.airtable.com/v0/appolcoyLfSXX3Xhy/QA`;
 
-
-    // Send GET request to Airtable API with authentication headers
-    const response = await axios.get(airtableUrl, {
+    // Add new question to Airtable table
+    await axios.post(airtableUrl, {
+      fields: {
+        Question: question,
+      },
+    }, {
       headers: {
         Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+        'Content-Type': 'application/json',
       },
     });
 
-    // If the response is successful, send back the answer field value as a JSON response
-    if (response.status === 200) {
-      const answer = response.data.records[0].fields.Answer;
+    // Wait up to 10 seconds for the answer to be populated
+    let answer;
+    for (let i = 0; i < 10; i++) {
+      const response = await axios.get(airtableUrl, {
+        params: {
+          maxRecords: 1,
+          filterByFormula: `AND({Question}="${question}", NOT({Answer}=""))`,
+        },
+        headers: {
+          Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+        },
+      });
+
+      if (response.data.records.length > 0) {
+        answer = response.data.records[0].fields.Answer;
+        break;
+      }
+
+      await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+
+    if (answer) {
+      // If the response is successful, send back the answer field value as a JSON response
       res.json({ answer });
     } else {
-      // Otherwise, send an error response
-      res.status(response.status).json({ error: 'An error occurred while fetching the answer.' });
+      // If no answer is found after 10 seconds, send a timeout error response
+      res.status(408).json({ error: 'A timeout error occurred while fetching the answer.' });
     }
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An internal server error occurred.' });
   }
 });
+
 
 // Start the server
 app.listen(port, () => {
