1515ab2a1be37bd41385e466bca31ef3b6f60441
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 16:36:27 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -61,64 +61,84 @@ function chatStripe(isAi, value, uniqueId) {
     `
     )
 }
-
 const handleSubmit = async (e) => {
-    e.preventDefault()
+  e.preventDefault()
 
-    const data = new FormData(form)
+  const data = new FormData(form)
 
-    // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
+  // user's chatstripe
+  chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
-    // log the user's question
-    console.log(`User question: ${data.get('prompt')}`);
+  // log the user's question
+  console.log(`User question: ${data.get('prompt')}`);
 
-    // to clear the textarea input 
-    form.reset()
+  // to clear the textarea input 
+  form.reset()
 
-    // bot's chatstripe
-    const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
+  // bot's chatstripe
+  const uniqueId = generateUniqueId()
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
-    // to focus scroll to the bottom 
-    chatContainer.scrollTop = chatContainer.scrollHeight;
+  // to focus scroll to the bottom 
+  chatContainer.scrollTop = chatContainer.scrollHeight;
 
-    // specific message div 
-    const messageDiv = document.getElementById(uniqueId)
+  // specific message div 
+  const messageDiv = document.getElementById(uniqueId)
 
-    // messageDiv.innerHTML = "..."
-    loader(messageDiv)
+  // messageDiv.innerHTML = "..."
+  loader(messageDiv)
 
-    const question = data.get('prompt')
-    const airtableUrl = `https://api.airtable.com/v0/appolcoyLfSXX3Xhy/QA?maxRecords=1&filterByFormula=AND({Question}="${question}")`;
+  const question = data.get('prompt')
+  const airtableUrl = 'https://api.airtable.com/v0/appolcoyLfSXX3Xhy/QA'
 
-
-    const response = await fetch(airtableUrl, {
-        headers: {
-            'Authorization': `Bearer keyO4UTbHbZ9n0vui`
+  const requestBody = {
+    records: [
+      {
+        fields: {
+          Question: question,
+          Answer: ''
         }
-    })
+      }
+    ]
+  }
+
+  const response = await fetch(airtableUrl, {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer keyO4UTbHbZ9n0vui`,
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(requestBody)
+  })
+
+  if (response.ok) {
+    // Wait for up to 60 seconds for the answer field to be populated
+    const startTime = Date.now()
+    let answer = ''
+
+    while (!answer && (Date.now() - startTime) < 60000) {
+      const response = await fetch(airtableUrl, {
+        headers: {
+          'Authorization': `Bearer keyO4UTbHbZ9n0vui`
+        }
+      })
+      
+      const data = await response.json()
+      const record = data.records.find(record => record.fields.Question === question)
+
+      if (record && record.fields.Answer) {
+        answer = record.fields.Answer
+      } else {
+        await new Promise(resolve => setTimeout(resolve, 1000))
+      }
+    }
 
     clearInterval(loadInterval)
     messageDiv.innerHTML = " "
-
-    if (response.ok) {
-        const data = await response.json();
-        const answer = data.records[0].fields.Answer.trim() // get the answer field value from the Airtable response
-
-        typeText(messageDiv, answer)
-    } else {
-        const err = await response.text()
-
-        messageDiv.innerHTML = "Something went wrong"
-        alert(err)
-    }
+    typeText(messageDiv, answer)
+  } else {
+    clearInterval(loadInterval)
+    messageDiv.innerHTML = "Something went wrong"
+    alert(response.statusText)
+  }
 }
-
-
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (e) => {
-    if (e.keyCode === 13) {
-        handleSubmit(e)
-    }
-})
