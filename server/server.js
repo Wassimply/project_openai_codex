30418069b6c3c4791bd24e855a9c3bf8849dcd80
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 12:18:09 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,22 +1,19 @@
-import express from 'express'
-import * as dotenv from 'dotenv'
-import cors from 'cors'
-import axios from 'axios'
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import axios from 'axios';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-app.use(cors())
-app.use(express.json())
-
-const MAX_RETRIES = 3;
-const RETRY_DELAY = 2000;
+const app = express();
+app.use(cors());
+app.use(express.json());
 
 app.get('/', async (req, res) => {
   res.status(200).send({
     message: 'Hello from Pure!'
-  })
-})
+  });
+});
 
 app.post('/', async (req, res) => {
   try {
@@ -25,35 +22,23 @@ app.post('/', async (req, res) => {
     const headers = {
       Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`
     };
-    
-    let retryCount = 0;
-    let answer = '';
-
-    while (retryCount < MAX_RETRIES) {
-      const response = await axios.get(airtableUrl, { headers });
+    const response = await axios.get(airtableUrl, { headers });
+    console.log('Response:', response.data);
+    if (response.data.records.length > 0) {
       const record = response.data.records[0];
-      
-      if (record && record.fields.Answer) {
-        answer = record.fields.Answer;
-        break;
-      }
-
-      retryCount++;
-      await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
-    }
-
-    if (answer) {
+      const answer = record.fields.Answer;
       res.status(200).send({
         bot: answer
       });
     } else {
-      res.status(500).send('No answer found');
+      res.status(404).send({
+        message: 'No answer found for that question'
+      });
     }
-
   } catch (error) {
-    console.error(error)
+    console.error(error);
     res.status(500).send(error || 'Something went wrong');
   }
-})
+});
 
-app.listen(5000, () => console.log('AI server started on http://localhost:5000'))
+app.listen(5000, () => console.log('AI server started on http://localhost:5000'));
