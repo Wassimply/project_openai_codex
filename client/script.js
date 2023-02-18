c61aedd83fabc1cc1d7bdbd959c09ed5b821b0a8
Wassim Zoghlami <wassim.zoghlami@gmail.com>
2023-02-18 20:42:05 +0100

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,69 +1,3 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
-import dotenv from 'dotenv';
-dotenv.config();
-
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
-
-let loadInterval;
-
-function loader(element) {
-  element.textContent = '';
-
-  loadInterval = setInterval(() => {
-    // Update the text content of the loading indicator
-    element.textContent += '.';
-
-    // If the loading indicator has reached three dots, reset it
-    if (element.textContent === '....') {
-      element.textContent = '';
-    }
-  }, 300);
-}
-
-function typeText(element, text) {
-  let index = 0;
-
-  let interval = setInterval(() => {
-    if (index < text.length) {
-      element.innerHTML += text.charAt(index);
-      index++;
-    } else {
-      clearInterval(interval);
-    }
-  }, 20);
-}
-
-// generate unique ID for each message div of bot
-// necessary for typing text effect for that specific reply
-// without unique ID, typing text will work on every element
-function generateUniqueId() {
-  const timestamp = Date.now();
-  const randomNumber = Math.random();
-  const hexadecimalString = randomNumber.toString(16);
-
-  return `id-${timestamp}-${hexadecimalString}`;
-}
-
-function chatStripe(isAi, value, uniqueId) {
-  return (
-    `
-    <div class="wrapper ${isAi && 'ai'}">
-        <div class="chat">
-            <div class="profile">
-                <img 
-                  src=${isAi ? bot : user} 
-                  alt="${isAi ? 'bot' : 'user'}" 
-                />
-            </div>
-            <div class="message" id=${uniqueId}>${value}</div>
-        </div>
-    </div>
-  `
-  );
-}
-
 const addNewQuestion = async (question) => {
   const airtableUrl = `https://api.airtable.com/v0/appolcoyLfSXX3Xhy/QA`;
 
@@ -89,7 +23,9 @@ const addNewQuestion = async (question) => {
   } catch (error) {
     console.error(error);
   }
-};const handleSubmit = async (e) => {
+};
+
+const handleSubmit = async (e) => {
   e.preventDefault();
 
   const data = new FormData(form);
